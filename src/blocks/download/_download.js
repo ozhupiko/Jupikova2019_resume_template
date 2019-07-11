5ff63e2c263581ce3b06e11d79b155eb6107e710
ozhupiko <olgazupikova@gmail.com>
2019-07-11 16:36:27 +0300

    add

--- src/blocks/download/_download.js
+++ src/blocks/download/_download.js
@@ -1,8 +1,8 @@
-var links;
+// var links;
 
-$('.btn--secondary_download').on("click", function download() {
+// $('.btn--secondary_download').on("click", function download() {
 
-    links = window.location.href + "JOHN_DOE_resume.7z";
-    return links;
+//     links = window.location.href + "JOHN_DOE_resume.7z";
+//     return links;
 
-});
+// });
